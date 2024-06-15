cadc38a8a1e02ca0cd4c590a6f95d401f9e1d8ac
TusharJarif10 <meheditushar76@gmail.com>
2024-06-15 16:10:17 +0600

    ddffdf

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -10,7 +10,7 @@ export default function Carousel({images}) {
 
     useEffect ( ()=>
     {
-        setTimeout( ()=> {slideRight()}, 3000)
+        setTimeout( ()=> {slideRight()}, 3500)
     }) 
     const slideRight = () => {
         if (current===images.length-1)
